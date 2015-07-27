3b2aa44ab2d22b2599e61545f902036af7486d2a
ryannyunn <ryannyunn@gmail.com>
2015-07-27 17:19:15 -0400

    fixed the infinite loop problem if there were no venues

--- app/models/search.rb
+++ app/models/search.rb
@@ -7,7 +7,7 @@ class Search
   def self.request_ll(query, ll)
     offset = 0
     venues = []
-    while venues.length < 20
+    while offset <= 500
       api_request = "https://api.foursquare.com/v2/venues/explore?client_id=#{ID}&client_secret=#{SECRET}&query=#{query}&ll=#{ll}&radius=1000&offset=#{offset}&limit=50&v=20140806&m=foursquare"
       api_response = open(api_request).read
       @response = JSON.parse(api_response)
@@ -16,6 +16,7 @@ class Search
           venues << venue
         end
       end
+      break if offset == 500 && venues.empty?
       offset += 50
     end
     venues
@@ -24,16 +25,16 @@ class Search
   def self.request_near(query, near)
     offset = 0
     venues = []
-    while venues.length < 5
+    while offset <= 500
       api_request = "https://api.foursquare.com/v2/venues/explore?client_id=#{ID}&client_secret=#{SECRET}&query=#{query}&near=#{near}&radius=1000&offset=#{offset}&limit=50&v=20140806&m=foursquare"
       api_response = open(api_request).read
       @response = JSON.parse(api_response)
       @response['response']['groups'][0]['items'].each do |venue|
         if (venue['venue']['stats']['usersCount'] != 0 && (venue['venue']['stats']['checkinsCount'])/(venue['venue']['stats']['usersCount']) > 2.5)
           venues << venue
-          binding.pry
         end
       end
+      break if offset == 500 && venues.empty?
       offset += 50
     end
     venues
